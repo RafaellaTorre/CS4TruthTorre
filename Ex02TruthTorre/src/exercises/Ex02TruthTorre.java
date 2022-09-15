3535045979a01ed7e5110af79d3040639d62a8c5
RafaellaTorre <94584823+RafaellaTorre@users.noreply.github.com>
2022-09-16 07:47:52 +0800

    Update Ex02TruthTorre.java

--- Ex02TruthTorre/src/exercises/Ex02TruthTorre.java
+++ Ex02TruthTorre/src/exercises/Ex02TruthTorre.java
@@ -16,15 +16,19 @@ public class Ex02TruthTorre {
     int random = (int) Math.floor(Math.random()*range)+1;
     int guesses = 5;
     int guessNum = 0;
+    char response = 'y';
+    int lowLim = -5;
+    int upLim = 5;
     //displays options to user
     System.out.println("Welcome to Higher or Lower! What will you do?\n" + "-Start game\n" + "-Change settings\n" + "-End application\n");
     String userChoice = sc.nextLine();
     
     switch(userChoice){
         case "Start game": //if user chooses to start game
+            do{
             System.out.println("You have " + guesses + " guess(es) left. What is your guess?");
             guessNum = sc.nextInt();
-            for (int i = 0; i < guesses; i++){
+            for (int i = 1; i < guesses; i++){
             int guessesLeft = guesses - i;
         
             if(guessNum < random){
@@ -46,11 +50,31 @@ public class Ex02TruthTorre {
             System.out.println("You lost...");
             }
             
-            else{
-            }
+            System.out.println("Play again?(y/n)");
+            response = sc.next().charAt(0);
+            }while(response == 'y');
         break;
-        
+
         case "Change settings": // if user chooses to change settings
+                   System.out.println("What is the lower limit of the random number?\n" + "What is the upper limit of the random number?\n" + "How many guesses are allowed?\n");
+                    String changeSet = sc.nextLine();
+                    System.out.println("Lower limit");
+                    lowLim = sc.nextInt();
+                              
+                    if(lowLim < 1){
+                    lowLim = lowLim*(-1);
+                       }
+                              
+                    else{
+                    }
+                              
+                    upLim = sc.nextInt();
+                            
+                    case "Guesses":
+                    int userGuess = sc.nextInt();
+                    guesses = userGuess;
+
+                    range = lowLim + upLim;
         break;
     
         case "End application": // if user chooses to end application
