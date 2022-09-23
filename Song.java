02f36d8c370ab300df11ad68edcc808fddd53419
RafaellaTorre <94584823+RafaellaTorre@users.noreply.github.com>
2022-09-23 08:56:40 +0800

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex03;
+
+/**
+ *
+ * @author Rafaella
+ */
+public class Song {
+    String title;
+    
+    public Song(String a){
+    title = a;
+    }
+}
